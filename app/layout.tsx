49f39e06fd5ae9f41d72fe48843c2f0972724626
Abdelrahmankhaled12 <abdelrahman.khaled1086@gmail.com>
2024-12-03 21:57:04 +0200

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import "./globals.css"; // Import global styles for the application
 import ButtonScroll from "@/components/buttonScroll/ButtonScroll"; // Importing ButtonScroll component for scroll-to-top functionality
 import { AOSInit } from "@/components/aos/AOSInit"; // Importing AOSInit for animations initialization
-import { Providers } from "./providers";
-import { Toaster } from "@/components/ui/toaster";
 
 // Define RootLayout component, which wraps the entire application layout
 export default function RootLayout({
@@ -18,10 +16,7 @@ export default function RootLayout({
       </head>
       <body>
         {/* Rendering the children passed to this layout */}
-        <Providers>
-          {children}
-        </Providers>
-        <Toaster />
+        {children}
 
         {/* Initialize AOS (Animate On Scroll) for scroll-based animations */}
         <AOSInit />
